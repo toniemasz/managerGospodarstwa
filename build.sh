c7a9c42c6d37c32475a8f292a74c6ab43e23d7e1
TomaszFirlej <tomaszf600@gmail.com>
2026-05-27 01:31:18 +0200

    .

--- build.sh
+++ build.sh
@@ -4,10 +4,16 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# ZMUSZAMY RANGERA DO WYGENEROWANIA SCHEMATU TABEL DLA TWOJEJ APLIKACJI
+python manage.py makemigrations sows
+python manage.py makemigrations
+
+# Uruchomienie migracji tabel do bazy Supabase
 python manage.py migrate
 
 if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
   python manage.py createsuperuser --noinput || echo "Superużytkownik już istnieje lub pominięto tworzenie."
 fi
 
-python manage.py collectstatic --noinput
+# Zbieranie plików CSS i JS
+python manage.py collectstatic --noinputput
