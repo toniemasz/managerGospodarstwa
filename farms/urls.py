02e7b4a766adc5b51ea06750cc0c16c666f7035f
Tomasz Firlej <107843864+toniemasz@users.noreply.github.com>
2026-06-17 23:06:51 +0200

    Add user data export URL

--- farms/urls.py
+++ farms/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from farms.views import farm_settings_view
+from farms.views import export_user_data_view, farm_settings_view
 
 
 urlpatterns = [
     path('', farm_settings_view, name='farm_settings'),
+    path('eksport-danych/', export_user_data_view, name='export_user_data'),
 ]
