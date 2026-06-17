2dd597432f6a2b9790ecc3c518d5afaf6ca5cb85
Tomasz Firlej <107843864+toniemasz@users.noreply.github.com>
2026-06-17 22:32:06 +0200

    Allow local HTTP sessions on develop

--- managerGospodarstwa/settings.py
+++ managerGospodarstwa/settings.py
@@ -36,8 +36,8 @@ CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
 
 if not DEBUG and not TESTING:
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    SESSION_COOKIE_SECURE = env.bool('SESSION_COOKIE_SECURE', default=True)
-    CSRF_COOKIE_SECURE = env.bool('CSRF_COOKIE_SECURE', default=True)
+    SESSION_COOKIE_SECURE = env.bool('SESSION_COOKIE_SECURE', default=False)
+    CSRF_COOKIE_SECURE = env.bool('CSRF_COOKIE_SECURE', default=False)
     SECURE_SSL_REDIRECT = env.bool('SECURE_SSL_REDIRECT', default=False)
 
 LOGIN_URL = 'login'
