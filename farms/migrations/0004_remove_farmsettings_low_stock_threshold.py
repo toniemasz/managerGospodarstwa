eff53dd576622fb53b2c76a5f593a73c40fe2976
Tomasz Firlej <107843864+toniemasz@users.noreply.github.com>
2026-06-17 23:09:59 +0200

    Remove farm low stock threshold migration

---
+++ farms/migrations/0004_remove_farmsettings_low_stock_threshold.py
@@ -0,0 +1,16 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('farms', '0003_farmsettingsmodel'),
+        ('feed', '0003_ingredient_low_stock_threshold'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='farmsettingsmodel',
+            name='low_stock_threshold_kg',
+        ),
+    ]
